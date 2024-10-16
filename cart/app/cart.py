8e48fa39524f17b281ea495bab8aa1b7c6234fa2
leriahes <valeriya-shevchenko-2002@mail.ru>
2024-10-16 23:24:59 +0700

    Сериализаторы

--- cart/app/cart.py
+++ cart/app/cart.py
@@ -1,19 +1,33 @@
 import pika, sys, os, json
 from sqlalchemy.orm import Session
 from app.database import get_db
-from app.models import Cart
+from app.models import Cart, Dish
 
 def get_cart(db: Session, user_id: str):
     cart = db.query(Cart).filter(Cart.user_id == user_id).first()
 
     if cart:
-        print(f" [i] Корзина пользователя {user_id}: {cart}")
+        print(f" [i] Корзина пользователя {user_id}: {cart.to_dict()}")
     else:
         print(f" [i] Корзина пользователя {user_id} не найдена")
 
 
-def add_to_cart(user_id):
-    print(f" [+] Добавление блюда в корзину пользователя {user_id}")
+def add_to_cart(db: Session, user_id: str, dish_name: str, dish_price: float):
+    cart = db.query(Cart).filter(Cart.user_id == user_id).first()
+
+    if not cart:
+        cart = Cart(user_id=user_id)
+        db.add(cart)
+        db.commit()
+        db.refresh(cart)
+
+    dish = Dish(name=dish_name, price=dish_price)
+    cart.dishes.append(dish)
+    cart.totalPrice += dish.finalPrice
+    db.commit()
+
+    print(f" [+] Блюдо {dish_name} добавлено в корзину пользователя {user_id}")
+
 
 def remove_from_cart(user_id):
     print(f" [-] Удаление блюда из корзины пользователя {user_id}")
@@ -31,7 +45,7 @@ def process_message(ch, method, properties, body):
     if action == 'get_cart':
         get_cart(db, user_id)
     elif action == 'add_to_cart':
-        add_to_cart(data['user_id'])
+        add_to_cart(db, user_id, "Пицца пепперони", 528.00)
     elif action == 'remove_from_cart':
         remove_from_cart(data['user_id'])
     
--- cart/app/models.py
+++ cart/app/models.py
@@ -6,6 +6,11 @@ class BaseModel(Base):
     __abstract__ = True
     id = Column(Integer, nullable=False, primary_key=True, index=True)
 
+    def to_dict(self, ordered_keys=None):
+        if ordered_keys:
+            return {key: getattr(self, key) for key in ordered_keys if hasattr(self, key)}
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
 class Cart(BaseModel):
     __tablename__ = 'carts'
 
@@ -17,6 +22,12 @@ class Cart(BaseModel):
     def __init__(self, user_id):
         self.user_id = str(user_id)
         self.totalPrice = 0.0
+
+    def to_dict(self):
+        ordered_keys = ['id', 'user_id', 'totalPrice']
+        cart_dict = super().to_dict(ordered_keys)
+        cart_dict['dishes'] = [dish.to_dict() for dish in self.dishes]
+        return cart_dict
 
 class Dish(BaseModel):
     __tablename__ = 'dishes'
@@ -45,6 +56,14 @@ class Dish(BaseModel):
         self.price = price
         self.finalPrice = price
 
+    def to_dict(self):
+        ordered_keys = ['id', 'name', 'category', 'size', 'price', 'finalPrice', 'sauce']
+        dish_dict = super().to_dict(ordered_keys)
+        dish_dict['products'] = [product.name for product in self.dish_products]
+        dish_dict['added_products'] = [product.to_dict() for product in self.added_products]
+        dish_dict['removed_products'] = [product.to_dict() for product in self.removed_products]
+        return super().to_dict(ordered_keys)
+
 class Product(BaseModel):
     __tablename__ = 'products'
 
@@ -54,6 +73,10 @@ class Product(BaseModel):
     dishes = relationship("Dish", secondary="dish_products", back_populates="dish_products")
     dishes_added = relationship("Dish", secondary="added_products", back_populates="added_products")
     dishes_removed = relationship("Dish", secondary="removed_products", back_populates="removed_products")
+
+    def to_dict(self):
+        ordered_keys = ['id', 'name', 'price']
+        return super().to_dict(ordered_keys)
 
 class AddedProduct(Base):
     __tablename__ = 'added_products'
